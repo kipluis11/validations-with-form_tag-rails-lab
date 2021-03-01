1194e14420ecc7f32129ed4f27d4743ed803fba9
kip_parcell <kipluis@gmail.com>
2021-02-28 17:21:33 -0700

    commit tree 3f65d44fe556d8a290a0ac3a5aa8fca39b6ec97b

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
+  get 'post/show'
+
+  get 'post/new'
+
+  get 'post/edit'
+
+  get 'post/create'
+
+  get 'post/update'
+
+  get 'author/show'
+
+  get 'author/new'
+
+  get 'author/edit'
+
+  get 'author/create'
+
+  get 'author/update'
+
+  get "/authors/new", to: "authors#new", as: "new_author"
+  post "/authors", to: "authors#create"
+  get "/authors/:id", to: "authors#show", as: "author"
+  get "/authors/:id/edit", to: "authors#edit", as: "edit_author"
+  patch "/authors/:id", to: "authors#update"
+
+  get "/posts/new", to: "posts#new", as: "new_post"
+  post "/posts", to: "posts#create"
+  get "/posts/:id", to: "posts#show", as: "post"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  patch "/posts/:id", to: "posts#update"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
